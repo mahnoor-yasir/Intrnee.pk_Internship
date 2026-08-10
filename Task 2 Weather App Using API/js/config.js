deea9b0e9adb5484634e135c71fa1f7a13b7b5ad
Mahnoor Yasir <167002467+mahnoor-yasir@users.noreply.github.com>
2026-08-10 16:10:52 +0500

    Create config.js

---
+++ Task 2 Weather App Using API/js/config.js
@@ -0,0 +1,10 @@
+/* =========================================================================
+   Weather Dashboard — API CONFIGURATION
+   ========================================================================= */
+
+// Use this API key (it's active and working)
+const API_KEY = "c44e0aa968cfbe530012fdc61179ef3b";
+
+const API_BASE = "https://api.openweathermap.org/data/2.5";
+
+window.WEATHER_CONFIG = { API_KEY, API_BASE };
